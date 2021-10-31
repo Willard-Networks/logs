35b3bb4450a18d5684f390e1a9bc271d4324febb
Otto Hooper <votton.hooper@gmail.com>
2021-10-31 19:10:08 +0000

    Fetch 50 pages of logs

--- src/util/database.ts
+++ src/util/database.ts
@@ -103,7 +103,7 @@ abstract class BaseDatabase {
         }
 
         logQuery += ` ORDER BY id DESC LIMIT ${
-            args.limit ? mysql.escape(args.limit).replace(/'/g, "") : 50
+            args.limit ? mysql.escape(args.limit).replace(/'/g, "") : 500
         };`;
         console.log(logQuery);
         return logQuery;
