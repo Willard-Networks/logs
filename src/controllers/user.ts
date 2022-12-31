ca521d9043db24a91d48ba1486ce228d195005ad
Otto Hooper <votton.hooper@gmail.com>
2022-12-31 14:13:12 +0200

    Rework logout method

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -15,7 +15,11 @@ export const account = (req: Request, res: Response): void => {
  * Logs out steam account.
  * @route GET /logout
  */
-export const logout = (req: Request, res: Response): void => {
-    req.logOut();
-    res.redirect("/");
+export const logout = (req: Request, res: Response, next: (arg0: never) => unknown): void => {
+    req.logout(function (err: unknown) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
 };
