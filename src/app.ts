b807c74c396725f4af687e543aacbd82424d6792
Otto Hooper <votton.hooper@gmail.com>
2024-11-24 15:38:56 +0000

    secure flag to true

--- src/app.ts
+++ src/app.ts
@@ -79,7 +79,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false, // Set to true only in production with HTTPS
+        secure: true,
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     },
